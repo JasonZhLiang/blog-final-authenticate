90fd161d51db497ac645596a2388a5a37dbc2019
jasonl <jasonl@bitheads.com>
2022-09-14 00:48:54 -0400

    take the adding new post permission for login user

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def contact():
 
 @app.route("/new-post", methods=['get', 'post'])
 @login_required
-# @admin_only
+@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
